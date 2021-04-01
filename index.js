cccc6050469abe40c8ba437c2355a8eb988da65a
jon bichar <jon.bichar@gmail.com>
2021-03-31 21:57:07 -0400

    graphql route

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const apollo_server_express_1 = require("apollo-server-express");
+// import { chats } from './db';
+const schema_1 = __importDefault(require("./schema"));
 const app = express_1.default();
+app.use(cors_1.default());
+app.use(express_1.default.json());
 app.get('/_ping', (req, res) => {
     res.send('pong');
 });
-app.get('/chats', (req, res) => {
+// app.get('/chats', (req, res) => {
+//   res.json(chats);
+// })
+const server = new apollo_server_express_1.ApolloServer({ schema: schema_1.default });
+server.applyMiddleware({
+    app,
+    path: '/graphql'
 });
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
